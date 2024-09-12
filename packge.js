ed250d8751f4bae8003e8331f990cdd87877e682
microtasker <microtasker465@gmail.com>
2024-09-13 01:37:14 +0500

    Add files via upload

---
+++ packge.js
@@ -0,0 +1,16 @@
+
+    function basic(){
+ document.getElementById('select').value="Basic";
+ document.getElementById("pkg").style.display="none"
+ document.getElementById("Confirm").style.display = "flex"; // Show the confirmation section
+    }
+    function Standard(){
+ document.getElementById('select').value="Standard";
+ document.getElementById("pkg").style.display="none"
+ document.getElementById("Confirm").style.display = "flex"; // Show the confirmation section
+    }
+    function Premium(){
+ document.getElementById('select').value="Premium";
+ document.getElementById("pkg").style.display="none"
+ document.getElementById("Confirm").style.display = "flex"; // Show the confirmation section
+    }
